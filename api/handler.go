23351718f93d15451431fdcf5c72c840afea371e
Kohei Suzuki <jingle@sfc.wide.ad.jp>
2018-12-23 16:03:55 +0900

    add error message

--- api/handler.go
+++ api/handler.go
@@ -34,57 +34,58 @@ func (s *Server) Exec(ctx context.Context, in *TestMsg) (*ReturnMsg, error) {
 		log.Println(".env is not specified")
 
 		if os.Getenv("ETH_SECRET_KEY") == "" {
-			log.Fatal("ETH_SECRET_KEY is not specified.")
+			log.Fatalln("ETH_SECRET_KEY is not specified.")
 		}
 	}
 
 	// setup ethereum client
-	client, err := ethclient.Dial("http://localhost:7545")
+	client, err := ethclient.Dial("http://localhost:8000")
 	if err != nil {
-		log.Fatal(err)
+		log.Fatalln("ethclient.Dial:", err)
 	}
 
 	privateKey, err := crypto.HexToECDSA(os.Getenv("ETH_SECRET_KEY"))
 	if err != nil {
-		log.Fatal(err)
+		log.Fatalln("crypto.HexToECDSA:", err)
 	}
 
 	publicKey := privateKey.Public()
 	publicKeyECDSA, ok := publicKey.(*ecdsa.PublicKey)
 
 	if !ok {
-		log.Fatal("error casting public key to ECDSA.")
+		log.Fatalln("error casting public key to ECDSA.")
 	}
 
 	fromAddress := crypto.PubkeyToAddress(*publicKeyECDSA)
 
 	nonce, err := client.PendingNonceAt(context.Background(), fromAddress)
 	if err != nil {
-		log.Fatal(err)
+		log.Fatalln("client.PendingNonceAt:", err)
 	}
 
 	gasPrice, err := client.SuggestGasPrice(context.Background())
 	if err != nil {
-		log.Fatal(err)
+		log.Fatalln("client.SuggestGasPrice:", err)
 	}
 
 	auth := bind.NewKeyedTransactor(privateKey)
 
 	auth.Nonce = big.NewInt(int64(nonce))
 	auth.GasLimit = uint64(GasLimit)
+	// auth.Value = big.NewInt(0)
 	auth.GasPrice = gasPrice
 
 	address := common.HexToAddress(os.Getenv("CONTRACT_ADDR"))
 	instance, err := contract.NewExampleContract(address, client)
 	if err != nil {
-		log.Fatal(err)
+		log.Fatalln("contract.NewExampleContract:", err)
 	}
 
 	switch in.Method {
 	case GET:
 		value, err := instance.Number(nil)
 		if err != nil {
-			log.Fatal("get", err)
+			log.Fatalln("GET:", err)
 		}
 
 		log.Printf("GET from %s\n", fromAddress.String())
@@ -94,11 +95,14 @@ func (s *Server) Exec(ctx context.Context, in *TestMsg) (*ReturnMsg, error) {
 		tx, err := instance.SetNumber(auth, uint32(in.Value))
 
 		if err != nil {
-			log.Fatal("add", err)
+			log.Fatalln("ADD:", err)
 		}
 
 		log.Printf("ADD from %s (value = %d)\n", fromAddress.String(), in.Value)
 		log.Printf("tx sent: %s\n", tx.Hash().Hex())
+
+		fmt.Println("auth.GasLimit", auth.GasLimit)
+		fmt.Println("auth.GasPrice", auth.GasPrice)
 		return &ReturnMsg{Msg: fmt.Sprintf(tx.Hash().Hex())}, nil
 	}
 
--- contract/tools/deploy.go
+++ contract/tools/deploy.go
@@ -17,6 +17,8 @@ import (
 
 const GasLimit uint = 4712388
 
+// const GasLimit uint = 2100000000
+
 func main() {
 	// load .env
 	err := godotenv.Load()
@@ -24,46 +26,47 @@ func main() {
 	if err != nil {
 		log.Println(".env is not specified.")
 		if os.Getenv("ETH_SECRET_KEY") == "" {
-			log.Fatal("ETH_SECRET_KEY is not specified.")
+			log.Fatalln("ETH_SECRET_KEY is not specified.")
 		}
 	}
 
-	client, err := ethclient.Dial("http://localhost:7545")
+	// client, err := ethclient.Dial("http://localhost:7545")
+	client, err := ethclient.Dial("http://localhost:8000")
 	if err != nil {
-		log.Fatal(err)
+		log.Fatalln("ethclient.Dial:", err)
 	}
 
 	privateKey, err := crypto.HexToECDSA(os.Getenv("ETH_SECRET_KEY"))
 	if err != nil {
-		log.Fatal(err)
+		log.Fatalln("crypto.HexToECDSA:", err)
 	}
 
 	publicKey := privateKey.Public()
 	publicKeyECDSA, ok := publicKey.(*ecdsa.PublicKey)
 	if !ok {
-		log.Fatal("error casting public key to ECDSA")
+		log.Fatalln("error casting public key to ECDSA")
 	}
 
 	fromAddress := crypto.PubkeyToAddress(*publicKeyECDSA)
 	nonce, err := client.PendingNonceAt(context.Background(), fromAddress)
 	if err != nil {
-		log.Fatal(err)
+		log.Fatalln("client.PendingNonceAt:", err)
 	}
 
 	gasPrice, err := client.SuggestGasPrice(context.Background())
 	if err != nil {
-		log.Fatal(err)
+		log.Fatalln("client.SuggestGasPrice:", err)
 	}
 
 	auth := bind.NewKeyedTransactor(privateKey)
 	auth.Nonce = big.NewInt(int64(nonce))
-	auth.Value = big.NewInt(0) // in wei
+	// auth.Value = big.NewInt(0) // in wei
 	auth.GasLimit = uint64(GasLimit)
 	auth.GasPrice = gasPrice
 
 	address, tx, _, err := contract.DeployExampleContract(auth, client)
 	if err != nil {
-		log.Fatal(err)
+		log.Fatalln("contract.DeployExampleContract:", err)
 	}
 
 	fmt.Println("Contract Address:", address.Hex())
